27fad9266fd3eac2d238d501a382799669c8bc0b
mudiyanmas <nithinluro16@gmail.com>
2025-05-02 13:56:58 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'LIZAMWOL',
+  author: '‎',
+  botName: "LIZAMWOL",
+  botOwner: 'MUDIYANMASS', // Your name
+  ownerNumber: '918137829228', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
