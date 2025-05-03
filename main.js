925ce58785fb83b6650a1b090b2d80c55325bbc0
TCRONEB HACKX <paidtechzone@gmail.com>
2025-05-03 15:05:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
+            newsletterJid: '120363401516239182@newsletter',
             newsletterName: 'LIZAMWOL',
             serverMessageId: -1
         }
